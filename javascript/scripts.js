9114d899ce48e1a43117deb1ec780150b21bb4e3
Charles Olivar <CharlesDavidOlivar@gmail.com>
2017-09-08 12:31:34 -0700

    fly in

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -8,10 +8,43 @@ function myFunction() {
     }
 }
 
+
+
+(function($) {
+
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+   $.fn.visible = function(partial) {
+
+      var $t            = $(this),
+      $w            = $(window),
+      viewTop       = $w.scrollTop(),
+      viewBottom    = viewTop + $w.height(),
+      _top          = $t.offset().top,
+      _bottom       = _top + $t.height(),
+      compareTop    = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+  };
+
+})(jQuery);
+
 var scrolltime = 500;
 
 $(document).ready(function() {
-    // $(".section-heading").css("color", "blue");
+
+    // Scrolling
     $("#home").click(function() {
         $('html, body').animate({
             scrollTop: $("#home_link").offset().top
@@ -42,4 +75,37 @@ $(document).ready(function() {
         }, scrolltime);
     });
 
+    // animate in
+    $(window).scroll(function(event) {
+
+      $(".module").each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+          el.addClass("come-in");
+      }
+  });
+
+  });
+
+    var win = $(window);
+    var allMods = $(".module");
+
+    // Already visible modules
+    allMods.each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("already-visible");
+    }
+    });
+
+    win.scroll(function(event) {
+
+      allMods.each(function(i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+          el.addClass("come-in");
+      }
+    });
+
+    });
 });
