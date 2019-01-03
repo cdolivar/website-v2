ac35b9b48d4d90e853134067661f62431dc381c0
cdolivar <charlesdavidolivar@gmail.com>
2019-01-02 22:06:29 -0800

    redoing with css grid // finished about

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,14 +1,11 @@
 function myFunction() {
     var x = document.getElementById("topnav");
-    console.log("clicked");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
         x.className = "topnav";
     }
 }
-
-
 
 (function($) {
 
